dbbd22611cfbff35f97b45950167d90497cc4039
omgbwa-yasse <omgbwayasse@gmail.com>
2026-07-15 06:26:13 -0500

    Initial Commit

---
+++ refuge/refugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  refugeApp.swift
+//  refuge
+//
+//  Created by user295774 on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct refugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
